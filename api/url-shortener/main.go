1979cea9e329549db3521851cdce27430278e617
Shobhit977 <shobhit00gupta@gmail.com>
2023-10-14 11:19:11 +0530

    feat:add url shortener logic

--- api/url-shortener/main.go
+++ api/url-shortener/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"context"
+	"crypto/sha1"
+	"encoding/base64"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) events.APIGatewayV2HTTPResponse {
+	svc, err := NewService()
+	if err != nil {
+		return events.APIGatewayV2HTTPResponse{}
+	}
+	log.Print(svc)
+	return events.APIGatewayV2HTTPResponse{}
+}
+func main() {
+	lambda.Start(handler)
+}
+
+// function to generate shortened url via sha1 algorithm
+func generateShortUrl(url string) string {
+	hash := sha1.New()
+	hash.Write([]byte(url))
+	sha1_hash := base64.URLEncoding.EncodeToString(hash.Sum(nil))
+	//retrun first 10 characters
+	return sha1_hash[:10]
+}
---
+++ api/url-shortener/service.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+type Response struct {
+	Status  int    `json:"statusCode"`
+	Message string `json:"message"`
+}
+
+type UrlInfo []struct {
+	Url      string `json:"url"`
+	ShortUrl string `json:"shorturl"`
+	Domain   string `json:"domain"`
+}
+type service struct {
+	sess *session.Session
+}
+
+func NewService() (service, error) {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("ap-south-1")},
+	)
+	if err != nil {
+		return service{}, err
+	}
+	return service{
+		sess: sess,
+	}, nil
+}
