e545f425bbf0f066a8ee1a0a8b1cbec4e613efb5
Shobhit977 <shobhit00gupta@gmail.com>
2023-10-14 11:37:56 +0530

    feat:add url parse logic

--- api/url-shortener/main.go
+++ api/url-shortener/main.go
@@ -4,22 +4,56 @@ import (
 	"context"
 	"crypto/sha1"
 	"encoding/base64"
+	"encoding/json"
+	"errors"
 	"log"
+	"net/url"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) events.APIGatewayV2HTTPResponse {
+func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 	svc, err := NewService()
 	if err != nil {
-		return events.APIGatewayV2HTTPResponse{}
+		return events.APIGatewayV2HTTPResponse{
+			Body: err.Error(),
+		}, nil
 	}
-	log.Print(svc)
-	return events.APIGatewayV2HTTPResponse{}
+	return shortenUrl(svc, req)
 }
-func main() {
-	lambda.Start(handler)
+
+func shortenUrl(svc service, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	if req.Body == "" {
+		return events.APIGatewayV2HTTPResponse{
+			Body: errors.New("Empty Request Body").Error(),
+		}, nil
+	}
+	var request Request
+	err := json.Unmarshal([]byte(req.Body), &request)
+	if err != nil {
+		return events.APIGatewayV2HTTPResponse{}, err
+	}
+	domain, err := parseUrl(request.Url)
+	if err != nil {
+		return events.APIGatewayV2HTTPResponse{
+			Body: "Invalid URL",
+		}, nil
+	}
+	log.Print(domain)
+	return events.APIGatewayV2HTTPResponse{}, nil
+
+}
+
+func parseUrl(inputUrl string) (string, error) {
+	parsedUrl, err := url.Parse(inputUrl)
+	if err != nil {
+		return "", err
+	}
+	replacer := strings.NewReplacer("www.", "", ".com", "")
+	domain := replacer.Replace(parsedUrl.Hostname())
+	return domain, nil
 }
 
 // function to generate shortened url via sha1 algorithm
@@ -29,4 +63,8 @@ func generateShortUrl(url string) string {
 	sha1_hash := base64.URLEncoding.EncodeToString(hash.Sum(nil))
 	//retrun first 10 characters
 	return sha1_hash[:10]
+}
+
+func main() {
+	lambda.Start(handler)
 }
--- api/url-shortener/service.go
+++ api/url-shortener/service.go
@@ -3,8 +3,12 @@ package main
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
+type Request struct {
+	Url string `json:"url"`
+}
 type Response struct {
 	Status  int    `json:"statusCode"`
 	Message string `json:"message"`
@@ -16,7 +20,8 @@ type UrlInfo []struct {
 	Domain   string `json:"domain"`
 }
 type service struct {
-	sess *session.Session
+	sess     *session.Session
+	s3Client *s3.S3
 }
 
 func NewService() (service, error) {
@@ -26,7 +31,9 @@ func NewService() (service, error) {
 	if err != nil {
 		return service{}, err
 	}
+	s3Client := s3.New(sess)
 	return service{
-		sess: sess,
+		sess:     sess,
+		s3Client: s3Client,
 	}, nil
 }
